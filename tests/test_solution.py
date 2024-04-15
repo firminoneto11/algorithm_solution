955c164c398038e6ea511c7a932a21afec536706
firminoneto11 <firminoneto11@hotmail.com>
2024-04-15 19:05:30 -0300

    test: added more test cases

--- tests/test_solution.py
+++ tests/test_solution.py
@@ -3,17 +3,65 @@ from pytest import mark
 from main import Contract, combine_orders, get_top_N_open_contracts
 
 
-def test_get_top_N_open_contracts():
-    contracts = [
-        Contract(1, 1),
-        Contract(2, 2),
-        Contract(3, 3),
-        Contract(4, 4),
-        Contract(5, 5),
-    ]
-
-    renegotiated, top_n, expected_open_contracts = [3], 3, [5, 4, 2]
-
+@mark.parametrize(
+    argnames="contracts,renegotiated,top_n,expected_open_contracts",
+    argvalues=(
+        (
+            [
+                Contract(1, 1),
+                Contract(2, 2),
+                Contract(3, 3),
+                Contract(4, 4),
+                Contract(5, 5),
+            ],
+            [3],
+            3,
+            [5, 4, 2],
+        ),
+        (
+            [
+                Contract(1, 1),
+                Contract(2, 2),
+                Contract(3, 3),
+                Contract(4, 4),
+                Contract(5, 5),
+            ],
+            [3],
+            1,
+            [5],
+        ),
+        (
+            [
+                Contract(1, 1),
+                Contract(2, 2),
+                Contract(3, 3),
+                Contract(4, 4),
+                Contract(5, 5),
+            ],
+            [3, 4],
+            2,
+            [5, 2],
+        ),
+        (
+            [
+                Contract(1, 1),
+                Contract(2, 2),
+                Contract(3, 3),
+                Contract(4, 4),
+                Contract(5, 5),
+            ],
+            [3, 4],
+            3,
+            [5, 2, 1],
+        ),
+    ),
+)
+def test_get_top_N_open_contracts(
+    contracts: list[Contract],
+    renegotiated: list[int],
+    top_n: int,
+    expected_open_contracts: list[int],
+):
     assert expected_open_contracts == get_top_N_open_contracts(
         contracts, renegotiated, top_n
     )
